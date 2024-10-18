e12d21500eec0b4db88b58a89fbeca784f82b50f
Faza Rama Nugraha <fazaramanugraha@gmail.com>
2024-10-18 16:04:27 +0700

    abstract

---
+++ constant.php
@@ -0,0 +1,20 @@
+<?php
+
+// define('NAMA', 'Faza Rama Nugraha');
+// echo NAMA;
+
+// echo "<br>";
+
+// const UMUR = 10;
+// echo UMUR;
+
+class Coba
+{
+  const NAMA = 'faza';
+}
+
+echo Coba::NAMA;
+echo '<br>';
+
+// ada magic constant
+echo __LINE__;
